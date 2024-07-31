28f048d4feae603908527e3199272c09613d0dad
MATHEUS BARROS RODRIGUES <mat43214@gmail.com>
2024-07-31 10:56:20 -0300

    feat: header component

--- matheus_portifolio/src/components/Header/Header.tsx
+++ matheus_portifolio/src/components/Header/Header.tsx
@@ -1,7 +1,27 @@
+"use client";
 import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+import NavBar from "../NavBar";
 
 function Header() {
-  return <div></div>;
+  return (
+    <header className="h-[15vh] bg-slate-950 blue flex flex-row px-[10%] items-center justify-between">
+      <Link href={"https://github.com/M12gthb"}>
+        <Image
+          src={"/Icon2.png"}
+          width={70}
+          height={70}
+          alt=""
+          className="cursor-pointer"
+        />
+      </Link>
+      {/* <h1 className="inline-block text-gradient text-stroke ">
+          Matheus Barros
+        </h1> */}
+      <NavBar />
+    </header>
+  );
 }
 
 export default Header;
