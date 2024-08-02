836e3600f8c257c98fc12ad8dacb0cd35e271365
MATHEUS BARROS RODRIGUES <mat43214@gmail.com>
2024-08-02 08:16:12 -0300

    raw: nextConfig add image domain "opengraph.githubassets.com"

--- matheus_portifolio/next.config.mjs
+++ matheus_portifolio/next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    images: {
+      domains: ['opengraph.githubassets.com'],
+    },
+  };
+  
+  export default nextConfig;
+  
