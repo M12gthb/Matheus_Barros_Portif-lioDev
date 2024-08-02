2a9e6da80fc2022bd6e363136473f977377242c5
MATHEUS BARROS RODRIGUES <mat43214@gmail.com>
2024-08-02 08:23:20 -0300

    feat: components About, Techs and Projects add

--- matheus_portifolio/src/app/page.tsx
+++ matheus_portifolio/src/app/page.tsx
@@ -1,5 +1,14 @@
+import About from "@/components/About";
+import Projects from "@/components/Projects";
+import Techs from "@/components/Techs";
 import Image from "next/image";
 
 export default function Home() {
-  return <main className="min-h-[85vh] bg-black"></main>;
+  return (
+    <main className=" bg-black min-h-[85vh]">
+      <About />
+      <Techs />
+      <Projects />
+    </main>
+  );
 }
