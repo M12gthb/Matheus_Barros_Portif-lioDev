90a2476a0b5e21ce1ff07de584e4654c458f1937
MATHEUS BARROS RODRIGUES <mat43214@gmail.com>
2024-08-02 08:19:38 -0300

    raw: add data from techs

---
+++ matheus_portifolio/src/app/data/techsItens.ts
@@ -0,0 +1,74 @@
+export const techsItens = [
+  {
+    name: "Html",
+    href: "/HTML.svg",
+  },
+  {
+    name: "Css",
+    href: "/CSS.svg",
+  },
+  {
+    name: "Javascript",
+    href: "/JS.svg",
+  },
+  {
+    name: "Typescript",
+    href: "/TS.svg",
+  },
+  {
+    name: "Git",
+    href: "/Git.svg",
+  },
+  {
+    name: "VSCode",
+    href: "/VSCode.svg",
+  },
+  {
+    name: "React.js",
+    href: "/React.svg",
+  },
+  {
+    name: "Next.js",
+    href: "/Next.png",
+  },
+  {
+    name: "Node.js",
+    href: "/Node.svg",
+  },
+  {
+    name: "express.js",
+    href: "/express.png",
+  },
+  {
+    name: "Nest.js",
+    href: "/Nest.svg",
+  },
+  {
+    name: "Python",
+    href: "/Python.svg",
+  },
+  {
+    name: "MongooDB",
+    href: "/Mongodb.svg",
+  },
+  {
+    name: "PostgeSql",
+    href: "/Postgrsql.svg",
+  },
+  //   {
+  //     name: "MySql",
+  //     href: "/msql.svg",
+  //   },
+  //   {
+  //     name: "Aws",
+  //     href: "/aws.svg",
+  //   },
+  {
+    name: "Sass",
+    href: "/sass.svg",
+  },
+  {
+    name: "Tailwindcss",
+    href: "/Tailwind.svg",
+  },
+];
