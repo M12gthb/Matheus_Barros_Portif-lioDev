8f50e0c46cdea27537c3a8461152b62f69240a42
MATHEUS BARROS RODRIGUES <mat43214@gmail.com>
2024-08-02 12:26:28 -0300

    feat: add Contacts

--- matheus_portifolio/src/app/page.tsx
+++ matheus_portifolio/src/app/page.tsx
@@ -1,4 +1,5 @@
 import About from "@/components/About";
+import Contacts from "@/components/Contacts";
 import Projects from "@/components/Projects";
 import Techs from "@/components/Techs";
 import Image from "next/image";
@@ -9,6 +10,7 @@ export default function Home() {
       <About />
       <Techs />
       <Projects />
+      <Contacts />
     </main>
   );
 }
