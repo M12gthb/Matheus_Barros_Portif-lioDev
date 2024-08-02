88e1298d5a9439fa78c4c56bd67289f1a99a5e8a
MATHEUS BARROS RODRIGUES <mat43214@gmail.com>
2024-08-02 08:14:49 -0300

    remove: delete api.ts

--- matheus_portifolio/src/services/api.ts
+++
@@ -1,9 +0,0 @@
-"use client";
-
-import axios from "axios";
-
-export const api = axios.create({
-  baseURL:
-    "https://api.github.com/users/M12gthb/repos?sort=created&direction=desc",
-  timeout: 20000,
-});
